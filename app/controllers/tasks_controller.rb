57455ad4d3c8cab69a32b44664bedbe8851afc02
Nicolas Grumberg <nicolas@vroomvroom.fr>
2022-11-15 15:40:35 +0100

    tasks 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,11 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to task_path, status: :see_other
+  end
   def create
     @task = Task.new(task_params)
     @task.save
@@ -18,14 +23,10 @@ class TasksController < ApplicationController
   end
   def update
     @task = Task.find(params[:id])
-    @task .update(task_params)
+    @task.update(task_params)
     redirect_to task_path(@task)
   end
-  def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-    redirect_to task_path, status: :see_other
-  end
+
   private
   def task_params
     params.require(:task).permit(:title, :details, :completed)
